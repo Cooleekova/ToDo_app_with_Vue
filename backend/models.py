c1e3387b2887233f6af1d1aa72082eee9b4e27bb
cooleekova <nushnushka@gmail.com>
2022-11-03 12:33:22 -0300

    Add new model Date field -> deadline

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
     text = models.CharField(max_length=40)
+    deadline = models.DateField()
     complete = models.BooleanField(default=False)
 
     def __str__(self):
