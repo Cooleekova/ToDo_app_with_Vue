b8cb92dfa8a2372793440f17a032bc9c2978141c
cooleekova <nushnushka@gmail.com>
2022-11-03 12:35:19 -0300

    Add deadline field validation

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Todo
-
+from datetime import date
 
 
 class ToDoSerializer(serializers.ModelSerializer):
@@ -8,4 +8,8 @@ class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = "__all__"
-        
+    
+    def validate_deadline(self, value):
+        if value < date.today():
+            raise serializers.ValidationError('This field must be an even number.')
+        return value
