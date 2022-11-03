1f306f9abea6782d00a0f151d386c8141c9c1846
cooleekova <nushnushka@gmail.com>
2022-11-03 12:37:31 -0300

    Create formatting date filter

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import moment from 'moment'
 
 Vue.config.productionTip = false;
 
@@ -10,3 +11,10 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('DD MMM')
+  }
+})
