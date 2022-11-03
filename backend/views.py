874bc7d86e50309bbf3ce9400b50c5b3852cc0ac
cooleekova <nushnushka@gmail.com>
2022-11-03 12:34:34 -0300

    Add deadline field

--- backend/views.py
+++ backend/views.py
@@ -10,12 +10,14 @@ class TODOViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for CRUD of TODO.
     """
-
-    queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
+    queryset = Todo.objects.all()
     filter_backends = [
       DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter,
     ]
-    filterset_fields = ("text", "complete")
-    search_fields = ("text")
-    ordering_fields = ("complete",)
+    filterset_fields = ("text", "complete", "deadline")
+    search_fields = ("text", "deadline")
+    ordering_fields = ("complete", "deadline")
+
+
+  
